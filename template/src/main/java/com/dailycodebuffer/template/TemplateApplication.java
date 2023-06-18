ac010d8080f2a0b70b1d067a74255c6a5da576fb
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2023-06-18 13:35:07 +0530

    Update TemplateApplication.java

--- template/src/main/java/com/dailycodebuffer/template/TemplateApplication.java
+++ template/src/main/java/com/dailycodebuffer/template/TemplateApplication.java
@@ -2,6 +2,12 @@ package com.dailycodebuffer.template;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ *  References: https://refactoring.guru/design-patterns/template-method
+ *              https://www.tutorialspoint.com/design_pattern/template_pattern.htm
+ */
+
+
 @SpringBootApplication
 public class TemplateApplication {
 
